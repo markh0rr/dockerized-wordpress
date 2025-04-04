2e033e5463e3c9bf1217fd90e8e0cf18d7e58cd1
Benoît MORAWIEC <78908194+B-mellon@users.noreply.github.com>
2025-04-04 11:28:51 +0200

    update the php config
    enables to successfully download the ocean wp demo theme

--- logic/config/entrypoint.sh
+++ logic/config/entrypoint.sh
@@ -3,11 +3,14 @@
 PHP_INI_PATH=$(php --ini | grep "Loaded Configuration File" | awk '{print $NF}')
 
 echo "memory_limit = 256M" >> $PHP_INI_PATH
-echo "max_execution_time = 300" >> $PHP_INI_PATH
-echo "upload_max_filesize = 50M" >> $PHP_INI_PATH
+echo "max_execution_time = 1200" >> $PHP_INI_PATH
+echo "upload_max_filesize = 100M" >> $PHP_INI_PATH
+echo "max_input_vars = 4000" >> $PHP_INI_PAT
+echo "max_input_time = 600" >> $PHP_INI_PAT
+echo "max_file_uploads = 20" >> $PHP_INI_PAT
+echo "post_max_size = 1000M" >> $PHP_INI_PAT
+echo "output_buffering = 4096" >> $PHP_INI_PAT
 
-echo "extension=gd" >> $PHP_INI_PATH
-echo "extension=curl" >> $PHP_INI_PATH
 echo "extension=xmlreader" >> $PHP_INI_PATH
 
 chmod -R 777 /wordpress/wp-content/
